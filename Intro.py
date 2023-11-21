7449b7b8d9f8c3356013b741e011c7008576d997
monsurat ayinde <mayinde08@gmail.com>
2023-11-21 23:24:46 +0100

    I added string methods

--- Intro.py
+++ Intro.py
@@ -1,2 +1,17 @@
-name=input("enter your name ")
-print("welcome " + name)
+fname =input("enter your first name ").upper()
+lname =input("enter your last name ").upper()
+print("welcome " + fname)
+print("your full name is " + fname+" " +lname)
+print("your full name can also be " + lname+" " +fname)
+lfname = len(fname)
+llname = len(lname)
+print("the length of your first name is  "+ str(lfname) + " characters" )
+print("the length of your last name is  "+ str(llname) + " characters" )
+ffchar = fname[0]
+lfchar = fname[-1]
+flchar = lname[0]
+llchar = lname[-1]
+print("the first character of your first name is " + ffchar)
+print("the last character of your first name is " + lfchar)
+print("the first character of your last name is " + flchar)
+print("the last character of your last name is " + llchar)
