67c98c501678f288fdf7165a898c2f3cb64bc366
monsurat ayinde <mayinde08@gmail.com>
2023-11-21 23:36:09 +0100

    modified intro.py in the fstring branch

--- Intro.py
+++ Intro.py
@@ -1,17 +1,17 @@
-fname =input("enter your first name ").upper()
-lname =input("enter your last name ").upper()
-print("welcome " + fname)
-print("your full name is " + fname+" " +lname)
-print("your full name can also be " + lname+" " +fname)
+fname =input("enter your first name ").title()
+lname =input("enter your last name ").title()
+print(f"welcome {fname}")
+print(f"your full name is {fname} {lname}")
+print(f"your full name can also be {lname} {fname}")
 lfname = len(fname)
 llname = len(lname)
-print("the length of your first name is  "+ str(lfname) + " characters" )
-print("the length of your last name is  "+ str(llname) + " characters" )
+print(f"the length of your first name is {lfname} characters" )
+print(f"the length of your last name is {llname} characters" )
 ffchar = fname[0]
 lfchar = fname[-1]
 flchar = lname[0]
 llchar = lname[-1]
-print("the first character of your first name is " + ffchar)
-print("the last character of your first name is " + lfchar)
-print("the first character of your last name is " + flchar)
-print("the last character of your last name is " + llchar)
+print(f"the first character of your first name is {ffchar}")
+print(f"the last character of your first name is {lfchar}")
+print(f"the first character of your last name is  {flchar}")
+print(f"the last character of your last name is {llchar}")
